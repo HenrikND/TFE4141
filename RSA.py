ae19338cabcb631d1f135f6545e81e999a88745e
henriknd <hnd007@hotmail.com>
2019-09-27 19:09:40 +0200

    update RSA

--- RSA.py
+++ RSA.py
@@ -1,13 +1,7 @@
-class montgomery:
-    p = 7
-    q = 17
-    e = 23
-    def __init__(self):
-        self.n=self.p*self.q
-        self.phi=(self.p-1)*(self.q-1)
-        self.calculate_d()
-        self.calculate_r()
-        self.calculate_r_mod_n()
+class montgomery_reduction:
+
+    def __init__(self, n, e):
+        pass
 
     def encrypt(self, M):
         # M^e mod n
@@ -23,19 +17,6 @@ class montgomery:
         else:
             print("function did not work!!", high_lvl_example, built_in_python_function)
 
-
-    def calculate_d(self):
-        j = 0
-        while 1:
-            j = j +1
-            d = self.phi-j
-            d=(d*self.e)
-
-            remainder = d%self.phi
-
-            if remainder == 1:
-                self.d = self.phi-j
-                return self.d
 
     def calculate_r(self):
         r = 2
@@ -57,12 +38,34 @@ class montgomery:
         return r_mod_n, r2_mod_n
 
     def calculate_k(self):
-        
+        pass
 
     def remainder(self, number, divisor):
         while number >= divisor:
             number -= divisor
         return number
 
+class RSA_keys():
+    p = 7
+    q = 17
+    e = 23
 
-print(montgomery().d)
+    def __init__(self):
+        self.n=self.p*self.q
+        self.phi=(self.p-1)*(self.q-1)
+        self.calculate_d()
+
+    def calculate_d(self):
+        j = 0
+        while 1:
+            j = j +1
+            d = self.phi-j
+            d=(d*self.e)
+
+            remainder = d%self.phi
+
+            if remainder == 1:
+                self.d = self.phi-j
+                return self.d
+
+
