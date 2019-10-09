a61b16e2819fcac90089473dbd9a33a66741a0c9
henriknd <hnd007@hotmail.com>
2019-10-09 17:33:05 +0200

    update monpro, not complete

--- monpro.vhdl
+++ monpro.vhdl
@@ -13,35 +13,46 @@ architecture arch of ent is
 
     signal S, P, Q : unsigned(255 downto 0);
     signal a : std_ulogic_vector(255 downto 0);
+    signal counter : std_ulogic_vector(9 downto 0)
 
 begin
 
     adder_stage_1 : process(clock, reset_n)
+        signal Q_buf : unsigned(255 downto 0);
+        signal state : std_ulogic;
     begin
         if reset_n = '0' then
-            -- not sure if needed
+           p <= (others  => '1');
+           Q_buf <= (others => '0');
         elsif rising_edge(clock) then
-            -- test case where the adding only happens when a(0) = '1'
-            if a(0) = '1' then
-                P = unsigned(b) + S;
-            else
-                P <= S
-            end if ;
+            case( state ) is
+                when '0' =>
+                    -- test case where the adding only happens when a(0) = '1'
+                    if a(0) = '1' then
+                        P = unsigned(b) + S;
+                    else
+                        P <= S
+                    end if ;
+
+                when '1' =>
+                    if P(0) = '1' then
+                        Q_buf <= unsigned(n) + P;
+                    else
+                        Q_buf <= P;
+                    end if ;
+                    Q <= Q_buf srl 1;
+            end case ;
         end if ;
     end process ; -- adder_stage_1
 
-
-    adder_stage_2 : process( clock, reset_n )
+    counter : process( clock,reset_n )
     begin
-        if rising_edge(clock) then
-            if P(0) = '1' then
-                Q <= std_ulogic(unsigned(n) + P);
-            else
-                Q <= P;
-            end if ;
-            Q <= Q srl 1;
+        if reset_n = '0' then
+            counter <= (others => '0');
         end if ;
-    end process ; -- adder_stage_2'
-
+        if rising_edge(clock) then
+            counter <= counter + 1;
+        end if ;
+    end process ; -- counter
 
 end arch ; -- arch
