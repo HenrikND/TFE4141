05685ca417c44b3055b94b762af51c451161cbfa
Henrik Nøsen Dahle <hnd007@hotmail.com>
2019-10-14 21:00:00 +0200

    Update monpro.vhd
    More stable config, added more reset and removed 1 register in data  path

--- monpro.vhd
+++ monpro.vhd
@@ -25,27 +25,35 @@ begin
 
     adder_stage_1 : process(clock, reset_n)
     begin
+        -- reset process for the montgomery product
         if reset_n = '0' then
-           p <= (others  => '0');
+           P <= (others  => '0');
            Q_buf <= (others => '0');
            counter <= (others => '0');
+           S <= (others => '0');
+           state <= '0';
+           a_shift <= (others => '0');
+           Q <= (others => '0');
+           result_buf <= (others => '0');
+
         -- Montgomery algorithm
         elsif rising_edge(clock) then
             if counter(8) = '1' then
                 if Q > unsigned(n) then
-                    result_buf <= Q - unsigned(n);
+                    result_buf <= ('0' & Q(255 downto 1)) - unsigned(n);
                 else
-                    result_buf <= Q;
+                    result_buf <= '0' & Q(255 downto 1);
                 end if ;
             else
                 -- shift register of a
-                if counter = 0 then
+                if counter = 0 then -- boot
                     a_shift <= a;
+                    S <= (others => '0');
                 else
                     a_shift(254 downto 0) <= a_shift(255 downto 1);
                     a_shift(255) <= '0';
                 end if ;
-
+                S <= '0' & Q(255 downto 1);
                 case( state ) is
                     when '0' =>
                         if a_shift(0) = '1' then
@@ -53,14 +61,14 @@ begin
                         else
                             P <= S;
                         end if ;
-
+                        state <= '1';
                     when '1' =>
                         if P(0) = '1' then
-                            Q_buf <= unsigned(n) + P;
+                            Q <= unsigned(n) + P;
                         else
-                            Q_buf <= P;
+                            Q <= P;
                         end if ;
-                        Q <= Q_buf srl 1;
+                        --Q <= Q(254 downto 0) & '0';
 
                         counter <= counter + 1;
                         state <= '0';
