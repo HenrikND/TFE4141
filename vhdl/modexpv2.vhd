2048b2324cf435aba0a7c66a252618303125ccc7
henriknd <hnd007@hotmail.com>
2019-10-23 23:57:36 +0200

    Update modexpv2.vhd

--- vhdl/modexpv2.vhd
+++ vhdl/modexpv2.vhd
@@ -24,28 +24,66 @@ architecture arch of modexp is
     end component;
 
     -- controll path signals
-    signal begin_monpro : std_logic;
-    signal state : std_logic_vector(4 downto 0);
+    signal monpro_begin : std_logic;
+    signal state : std_logic_vector(3 downto 0);
+    signal counter : std_logic_vector(8 downto 0);
 
     -- data path signals
+    signal s_mon : std_logic_vector(255 downto 0);
+    signal c :std_logic_vector(255 downto 0);
+    signal e_shift : std_logic_vector(255 downto 0);
+
   begin
     monpro : monprov2 port map(clock => clock,
                               reset_n => reset_n,
-                              begin_monpro => begin_monpro,
-                              a => P,
-                              b => m ,
-                              n => n,
-                              result => s_mon,
-                              done => done_2);
-
-
+                              begin_monpro => monpro_begin,
+                              a => monpro_a,
+                              b => monpro_b ,
+                              n => monpro_n,
+                              result => monpro_result,
+                              done => monpro_done);
 
     controller : process( clock, reset_n )
     begin
       if( reset_n = '0' ) then
+        state <= (others => '0');
+
 
       elsif( rising_edge(clock) ) then
+        -- finite state machine
+        case( state ) is
 
+          when "0000" =>
+
+            monpro_begin <= '1';
+            state <= "0001";
+
+          when "0001" =>
+            monpro_begin <= '0';
+            if monpro_done then
+              state <= "0100"
+            end if ;
+          when "0100" =>
+
+          when "0101" =>
+
+          when "0110" =>
+
+          when "0111" =>
+
+          when "1000" =>
+
+          when "1001" =>
+
+          when "1111" =>
+
+          when others =>
+
+        end case ;
+
+        if state() then
+          
+        end if ;
       end if ;
     end process ; -- controller
 
