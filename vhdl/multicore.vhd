18badc38dbd55f5b7df389d8212950a8277ded35
Henrik Nøsen Dahle <hnd007@hotmail.com>
2019-10-28 19:04:46 +0100

    Update multicore.vhd
    working tecniqe for creating many object in few lines

--- vhdl/multicore.vhd
+++ vhdl/multicore.vhd
@@ -4,6 +4,7 @@ use ieee.numeric_std.all;
 
 
 entity multicore is
+    generic  (number_of_cores : integer := 5);
     port (
         clock, reset_n : in std_logic;
         e, p, n, m, p_mon : in std_logic_vector(255 downto 0);
@@ -13,7 +14,7 @@ entity multicore is
 end multicore ;
 
 architecture arch of multicore is
-    component modexpv2 is
+    component modexp is
         port (
             clock, reset_n : in std_logic;
             e, p, n, m, p_mon : in std_logic_vector(255 downto 0);
@@ -22,51 +23,32 @@ architecture arch of multicore is
           ) ;
       end component;
 
-      signal input_state, output_state : std_logic_vector(4 downto 0);
+    --g_gen_sig: for ii in 0 to 7 generate
+      -- signal modexp_m_ii : std_logic_vector(255 downto 0);
+      -- begin
+    --end generate g_gen_sig;
       -- modexp conections
-      signal modexp_m_1, modexp_data_1, modexp_ready_1 : std_logic_vector(255 downto 0);
+      --signal modexp_0,modexp_data_0,modexp_ready_0 : std_logic_vector(255 downto 0);
+      --signal modexp_1,modexp_data_1,modexp_ready_1 : std_logic_vector(255 downto 0);
+      --signal modexp_2,modexp_data_2,modexp_ready_2 : std_logic_vector(255 downto 0);
+      --signal modexp_3,modexp_data_3,modexp_ready_3 : std_logic_vector(255 downto 0);
+
+      type mem is array (0 to number_of_cores) of std_logic_vector(255 downto 0);
+      signal modexp_m ,modexp_data: mem;
+      signal modexp_ready : std_logic_vector(number_of_cores downto 0);
 begin
 
--- modexp blocks
-modexp_1 : modexpv2 port map( clock => clock,
+-- generate the nessesary number of blocks
+g_gen_modexp: for i in 0 to number_of_cores generate
+    uut: modexp port map( clock => clock,
                             reset_n => reset_n,
                             e => e,
                             n => n,
                             p => p,
-                            m => modexp_m_1,
+                            m => modexp_m(i),
                             p_mon => p_mon,
-                            data_out => modexp_data_1,
-                            data_ready => modexp_ready_1);
-
-
-
-input_state_machine : process( clock, reset_n )
-begin
-  if( reset_n = '0' ) then
-
-  elsif( rising_edge(clock) ) then
-    case( input_state ) is
-
-        when IDLE =>
-
-
-        when others =>
-
-    end case ;
-  end if ;
-end process ; -- input_state_machine
-
-
-
-output_state_machine : process( clock, reset_n )
-begin
-  if( reset_n = '0' ) then
-
-  elsif( rising_edge(clock) ) then
-
-  end if ;
-end process ; -- output_state_machine
-
-
+                            data_out => modexp_data(i),
+                            data_ready => modexp_ready(i));
+   end generate g_gen_modexp;
 
 end architecture ; -- arch
