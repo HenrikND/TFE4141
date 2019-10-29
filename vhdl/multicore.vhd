7a6aaaecd0e0a5656a0bd2027e8c19b4536bc5cf
henriknd <hnd007@hotmail.com>
2019-10-29 21:17:43 +0100

    update to the mulitcore

--- vhdl/multicore.vhd
+++ vhdl/multicore.vhd
@@ -6,7 +6,7 @@ use ieee.numeric_std.all;
 entity multicore is
     generic  (number_of_cores : integer := 5);
     port (
-        clock, reset_n , last_message_in, message_in_ready: in std_logic;
+        clock, reset_n , last_message_in, message_in_ready, message_out_ready: in std_logic;
         e, p, n, m, p_mon : in std_logic_vector(255 downto 0);
         data_out : out std_logic_vector(255 downto 0);
         data_ready : out std_logic
@@ -14,6 +14,7 @@ entity multicore is
 end multicore ;
 
 architecture arch of multicore is
+    -- componets used in architecture
     component modexp is
         port (
             clock, reset_n: in std_logic;
@@ -21,28 +22,32 @@ architecture arch of multicore is
             data_out : out std_logic_vector(255 downto 0);
             data_ready : out std_logic
           ) ;
-      end component;
+    end component;
 
-    --g_gen_sig: for ii in 0 to 7 generate
-      -- signal modexp_m_ii : std_logic_vector(255 downto 0);
-      -- begin
-    --end generate g_gen_sig;
-      -- modexp conections
-      --signal modexp_0,modexp_data_0,modexp_ready_0 : std_logic_vector(255 downto 0);
-      --signal modexp_1,modexp_data_1,modexp_ready_1 : std_logic_vector(255 downto 0);
-      --signal modexp_2,modexp_data_2,modexp_ready_2 : std_logic_vector(255 downto 0);
-      --signal modexp_3,modexp_data_3,modexp_ready_3 : std_logic_vector(255 downto 0);
+    -- type definitions
+    type data_arr_t is array (0 to number_of_cores) of std_logic_vector(255 downto 0);
+    type flag_t is std_logic_vector(number_of_cores downto 0);
 
-      type mem is array (0 to number_of_cores) of std_logic_vector(255 downto 0);
-      signal modexp_m ,modexp_data: mem;
-      signal modexp_ready, modexp_reset_n: std_logic_vector(number_of_cores downto 0);
-      signal modexp_state : integer;
-      signal input_thing : std_logic_vector(1 downto 0);
+    -- modexp connections
+    signal modexp_m             : data_arr_t;
+    signal modexp_data          : data_arr_t;
+    signal modexp_ready         : flag_r;
+    signal modexp_reset_n       : flag_r;
+
+    -- control signals
+    signal modexp_state         : integer;
+    signal input_state          : std_logic_vector(1 downto 0);
+    signal output_state         : std_logic_vector(1 downto 0);
+
+    -- modexp flags
+    signal flag_last_message    : flag_t;
+    signal flag_busy            : flag_t;
+    --signal flag_number          : integer;
 
 begin
 
 -- generate the nessesary number of blocks
-    g_gen_modexp: for i in 0 to number_of_cores generate
+    g_gen_modexp: for i in 0 to number_of_cores -1 generate
         uut: modexp port map( clock => clock,
                             reset_n => modexp_reset_n(i),
                             e => e,
@@ -52,13 +57,18 @@ begin
                             p_mon => p_mon,
                             data_out => modexp_data(i),
                             data_ready => modexp_ready(i));
+        -- when busy is 1 the modexp core should function as normal but when 0 the core should be in a reset state
+        modexp_reset_n(i) <= flag_busy(i) or reset_n;
     end generate g_gen_modexp;
+
+
+
 
     controll : process( clock, reset_n )
     begin
         if( reset_n = '0' ) then
             modexp_state <= 0;
-            input_thing <= "00";
+            input_state <= "00";
             modexp_reset_n <= (others => '0');
         elsif( rising_edge(clock) ) then
             -- check if input state has reach max
@@ -66,37 +76,43 @@ begin
                 modexp_state <= 0;
             end if;
 
-            case(input_thing) is
+            -- good chance needs to be changed, maybe two stated instead, run and finish
+            case(input_state) is
             -- boot state
             when "00" =>
                 if modexp_state > number_of_cores then
-                    input_thing <= "01";
+                    input_state <= "01";
                 end if;
             --looping over messages
             when "01" =>
                 if last_message_in = '1' then
-                    input_thing <= "10";
+                    input_state <= "10";
                 end if;
             -- stoping since reach last message
             when "10" =>
 
             when others =>
-                input_thing <= "00";
+                input_state <= "00";
             end case;
         end if ;
 
         -- push message
-        if input_thing = "00" then
-            if message_in_ready = '1' then
-                modexp_m(modexp_state) <= m;
+        if flag_busy(input_state) = '0' then
+            modexp_m(input_state) <= m;
+            input_state <= input_state + 1;
+        end if ;
 
-                modexp_reset_n(modexp_state) <= '1';
-            end if;
+        -- check if core is done and send data out
+        if flag_busy(output_state) = '1' and modexp_ready(output_state) = '1' then
+            data_out <= modexp_data(output_state);
+            data_ready <= '1';
+            if message_out_ready  = '1' then
+                output_state <= output_state + 1;
+                modexp_busy(output_state) <= '0'
+            end if ;
         else
-            if modexp_ready(modexp_state) = '1' then
-
-            end if;
-        end if;
+            data_ready <= '0';
+        end if ;
     end process ; -- controll
 
 end architecture ; -- arch
